9eada017c06267e5804077afc16fde25258b256a
vishakha sharma <vishakha.sharma@locusrags.com>
2019-06-19 13:36:48 +0530

    pdf generated

--- documents/admin.py
+++ documents/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
-from documents.models import Document
+from documents.models import Document, File
 
 
 
 # Register your models here.
 
+class FileInline(admin.TabularInline):
+    model = File
+
 class DocumentAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated', 'key')
     prepopulated_fields = {'slug': ('title',)}
+    inlines = [
+        FileInline,
+    ]
+
+
+
 
 
 admin.site.register(Document, DocumentAdmin)
