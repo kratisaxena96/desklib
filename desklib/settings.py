188cdfb8dc75466ae88688f6e4a144bdfc668e98
Rishi Dutta <rishi.dutta@zucolservices.com>
2019-07-11 16:37:43 +0530

    Changes in the search to make it executeable

--- desklib/settings.py
+++ desklib/settings.py
@@ -116,8 +116,10 @@ JSON_LD_INVALID_SD = 'throw'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'desklib_dev_db',
+        'USER': 'root',
+        'PASSWORD': 'locus123',
     }
 }
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
