f15204cfb3cb55f9de4a052d81ead7d4f0d66635
Rishi Dutta <rishi.dutta@zucolservices.com>
2019-07-15 18:52:12 +0530

    Admin Site for all backend modules

--- subjects/admin.py
+++ subjects/admin.py
@@ -5,8 +5,12 @@ from subjects.models import Subject
 
 
 class SubjectAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['title','description']
+    list_display = ('name','tag_list')
+    search_fields = ['name','description']
 
+    def tag_list(self, obj):
+        return u", ".join(o.name for o in obj.keywords.all())
 
 admin.site.register(Subject, SubjectAdmin)
 
