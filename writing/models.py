4c8f1a1a9e15ff23af455007cd110295af115835
kushagra <kushagra.goel@locusrags.com>
2019-07-23 12:37:53 +0530

    added block for google analytics, need to add file with code removed models, migrations from writing app separated settings files

--- writing/models.py
+++ writing/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class Compare(models.Model):
-    textarea1 = models.CharField(max_length=1000, blank=True, null=True)
-    textarea2 = models.CharField(max_length=1000, blank=True, null=True)
-
-
-class Spell(models.Model):
-    spell_textarea = models.CharField(max_length=1000, blank=True, null=True)
