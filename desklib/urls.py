aff8049e083f97b29639484f8dc42ea00a118a9b
nishi7796 <nishi.kumari@locuzgroup.com>
2019-08-08 13:53:13 +0530

    Subscription URL corrected and added to a tag.

--- desklib/urls.py
+++ desklib/urls.py
@@ -54,7 +54,7 @@ urlpatterns = [
     path('sitemap-<section>.xml', views.sitemap, {'sitemaps': sitemaps},
                        name='django.contrib.sitemaps.views.sitemap'),
     # path('payment/doc', PaypalPaymentView.as_view(), name='paypal_view'),
-    path('subscription/', SubscriptionView.as_view(), name='payment'),
+    path('subscription/', SubscriptionView.as_view(), name='subscription'),
     path('paynow/', PayNowView.as_view(), name='paynow'),
     path('paypal/', include('paypal.standard.ipn.urls'), name='paypal-ipn'),
     path('api-auth/', include('rest_framework.urls')),
