f78ae2b309c9fc7dae31462b7f1b2f0ec1185e90
Menka Kumari <nishi.kumari@zucolservices.com>
2019-08-26 10:50:15 +0530

    added share on linkedin links

--- samples/urls.py
+++ samples/urls.py
@@ -1,10 +1,10 @@
-# from django.urls import path, include
-# from .views import SampleView, SampleListView
-# from django.views.generic import TemplateView
-#
-#
-# urlpatterns = [
-#     path('', SampleListView.as_view(), name='sample-list-view'),
-#     path('resume/<slug:slug>/', SampleView.as_view(), name='sample-view'),
-#
-# ]
+from django.urls import path, include
+from .views import SampleView, SampleListView
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    path('', SampleListView.as_view(), name='sample-list-view'),
+    path('resume/<slug:slug>/', SampleView.as_view(), name='sample-view'),
+
+]
