6faaba3dc5acf5a2c57dab0da17a92a3f8c4fe21
siddharth <siddharthjhanwar@yahoo.in>
2019-08-28 13:39:12 +0530

    BUGFIX: cannot import name 'ALLOWED_IPS'

--- desklib/mixins.py
+++ desklib/mixins.py
@@ -1,14 +1,12 @@
-import socket
-
+from django.conf import settings
 from django.http import Http404
-from desklib.settings.dev_settings import ALLOWED_IPS
 from desklib.utils import get_client_ip
 
 
 class RestrictIpMixin(object):
     def dispatch(self, request, *args, **kwargs):
         ipAddr = get_client_ip(request)
-        if ipAddr in ALLOWED_IPS:
+        if ipAddr in settings.ALLOWED_IPS:
             return super(RestrictIpMixin, self).dispatch(request, *args, **kwargs)
         else:
             raise Http404
