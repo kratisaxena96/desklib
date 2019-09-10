82c50d6118a7dc31c9a4305043078254c0d6f36b
kushagra <kushagra.goel@locusrags.com>
2019-09-10 15:42:32 +0530

    api url changed

--- documents/api_urls.py
+++ documents/api_urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .api_views import DocumentCreateApiView, ReportDocumentApi, DocumentFeedbackApi
 
 urlpatterns = [
-    path('create/document/', DocumentCreateApiView.as_view(), name='document-create-api'),
+    path('create/', DocumentCreateApiView.as_view(), name='document-create-api'),
     path('report-document-api/', ReportDocumentApi.as_view(), name='report-document-api'),
     path('document-feedback-api/<slug:slug>/', DocumentFeedbackApi.as_view(), name='document-feedback-api'),
 
