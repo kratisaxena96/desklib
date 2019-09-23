4648493abecbe1f73126df73a23efd0847d1de93
kushagra <kushagra.goel@locusrags.com>
2019-09-23 16:08:42 +0530

    my-profile updated

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,5 +8,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', include('allauth.urls')),
-    path('my-profile', MyProfileView.as_view(), name='my-profile'),
+    path('my-profile/', MyProfileView.as_view(), name='my-profile'),
 ]
