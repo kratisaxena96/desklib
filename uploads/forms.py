c0ff8f7f8722d4e6ca181dda01b3cdd4b07eb91c
Rishi Dutta <rishi.dutta@zucolservices.com>
2019-10-04 10:34:13 +0530

    File upload by user and my-uploads

--- uploads/forms.py
+++ uploads/forms.py
@@ -7,8 +7,8 @@ from django.db.models import Q
 
 class UploadForm(forms.ModelForm):
     course_name = forms.CharField(label='Course Name', widget=forms.TextInput(attrs={'class': "form-control"}), required=True)
-    subjects = forms.ChoiceField(label='Subject', choices=Subject, widget=forms.Select(attrs={'class': "form-control"}), required=True)
-    type = forms.ChoiceField(label='Type of Document', choices=Upload.TYPE_OF_DOCUMENT, widget=forms.Select(attrs={'class': "form-control"}), required=True)
+    # subjects = forms.ChoiceField(label='Subject',initial=0, widget=forms.Select(attrs={'class': "form-control"}), required=True)
+    type = forms.ChoiceField(label='Type of Document', choices=Upload.TYPE_OF_DOCUMENT, widget=forms.Select(attrs={'class': "form-control"}))
     upload_file = forms.FileField(label='Upload Document', widget=forms.FileInput(attrs={'class': "form-control"}), required=True)
     course_code = forms.CharField(label='Course Code', widget=forms.TextInput(attrs={'class': "form-control"}), required=False)
     country = CountryField().formfield(blank_label='(select country)', widget=CountrySelectWidget(attrs={'class': "form-control"}))
@@ -16,8 +16,8 @@ class UploadForm(forms.ModelForm):
 
     class Meta:
         model = Upload
-        fields = ['course_name', 'course_code', 'country', 'university', 'type', 'upload_file', 'subjects']
+        fields = ['course_name', 'course_code','upload_file','country', 'university', 'type', 'subjects']
 
-    def __init__(self, *args, **kwargs):
-        super(UploadForm, self).__init__(*args, **kwargs)
-        self.fields['subjects'].queryset = Subject.objects.filter(~Q(parent_subject=None))
+        def __init__(self, *args, **kwargs):
+            super(UploadForm, self).__init__(*args, **kwargs)
+            self.fields['subjects'].queryset = Subject.objects.filter(~Q(parent_subject=None))
--- uploads/views.py
+++ uploads/views.py
@@ -1,12 +1,59 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView, FormView, ListView
+from django_json_ld.views import JsonLdContextMixin
+
 from uploads.models import Upload
 from uploads.forms import UploadForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
+from uploads.models import Upload
 
-
-class UploadDocumentView(LoginRequiredMixin, CreateView):
+class UploadDocumentView(LoginRequiredMixin, JsonLdContextMixin, CreateView):
     form_class = UploadForm
     model = Upload
     template_name = 'uploads/upload_document.html'
+    success_url = "/"
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.author = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+class MyUploads(LoginRequiredMixin, ListView):
+    model = Upload
+    template_name = "uploads/my_uploads.html"
+    paginate_by = 10
+    title = "My-uploads"
+
+    def get_queryset(self):
+        queryset = super(MyUploads, self).get_queryset()
+        queryset = queryset.filter(author=self.request.user).order_by('created')
+        return queryset
+#     def get(self, request, *args, **kwargs):
+
+
+#         my_uploads = Upload.objects.filter(user=self.request.user).order_by('created_at')
+#         paginator = Paginator(user_downloads, 6)
+#         page = request.GET.get('page1')
+#         try:
+#             user_downloads = paginator.page(page)
+#         except PageNotAnInteger:
+#             user_downloads = paginator.page(1)
+#         except EmptyPage:
+#             user_downloads = paginator.page(paginator.num_pages)
+#
+#         page_view = PageView.objects.filter(user=self.request.user).order_by('created_at')
+#         paginator = Paginator(page_view, 6)
+#         page = request.GET.get('page2')
+#         try:
+#             page_view = paginator.page(page)
+#         except PageNotAnInteger:
+#             page_view = paginator.page(1)
+#         except EmptyPage:
+#             page_view = paginator.page(paginator.num_pages)
+#
+#         context = {'user_downloads': user_downloads, 'page_view': page_view}
+#
+#         return self.render_to_response(context)
