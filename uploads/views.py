c87873438db062e0a8096b89af12b0ac325f2ecf
Rishi Dutta <rishi.dutta@zucolservices.com>
2019-10-07 15:45:33 +0530

    my-uploads fix

--- uploads/views.py
+++ uploads/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import CreateView, TemplateView, FormView, ListView
 from django_json_ld.views import JsonLdContextMixin
+from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage
+from django.core.paginator import PageNotAnInteger
 
 from uploads.models import Upload
 from uploads.forms import UploadForm
@@ -24,10 +27,27 @@ class UploadDocumentView(LoginRequiredMixin, JsonLdContextMixin, CreateView):
 class MyUploads(LoginRequiredMixin, ListView):
     model = Upload
     template_name = "uploads/my_uploads.html"
-    paginate_by = 10
+    paginate_by = 6
     title = "My-uploads"
 
     def get_queryset(self):
         queryset = super(MyUploads, self).get_queryset()
         queryset = queryset.filter(author=self.request.user).order_by('created')
         return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(MyUploads, self).get_context_data(**kwargs)
+        upload = Upload.objects.filter(author=self.request.user).order_by('created')
+        paginator = Paginator(upload, self.paginate_by)
+
+        page = self.request.GET.get('page')
+
+        try:
+            file_exams = paginator.page(page)
+        except PageNotAnInteger:
+            file_exams = paginator.page(1)
+        except EmptyPage:
+            file_exams = paginator.page(paginator.num_pages)
+
+        context['object'] = file_exams
+        return context
