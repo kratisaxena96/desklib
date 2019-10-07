1455b1e108dce91c6c992d820c7179dae5c02313
Rishi Dutta <rishi.dutta@zucolservices.com>
2019-10-07 15:02:08 +0530

    upload files after finding duplicates

---
+++ upload_files_after_duplicates.py
@@ -0,0 +1,114 @@
+import os
+
+import selenium
+import textract
+from documents.utils import get_text
+import requests
+from termcolor import colored
+import sqlite3
+from sqlite3 import Error
+# command to run the file
+# exec(open("./upload_files_after_duplicates.py").read())
+
+path = input("Folder Path: ")
+last_file = input("Enter last filename to restart program: ")
+# url = input("Enter Url Of website: ")
+# url = 'http://127.0.0.1:8000/api/document/create/'
+url = 'http://127.0.0.1:8000/api/document/create/'
+# ACC-201-Accounting-Cycle-Report-Template1docx-7178.docx
+ALLOWED_EXTENSIONS = ['pdf','docx','doc', 'pptx', 'ppt', 'odt', 'odf']
+IGNORE_FILENAME_KEYWORDS = ['lecture', 'rubic', 'rubric', 'instructions', 'assignmentbrief', 'criteria', 'requirements', 'programmingassignment', 'individual-assignment', 'individualassignment']
+IGNORE_FILECONTENT_KEYWORDS = ['ORIGINALITY REPORT']
+
+import hashlib
+conn = None;
+try:
+    conn = sqlite3.connect(':memory:')
+except Error as e:
+    print(e)
+try:
+        c = conn.cursor()
+        c.execute("DROP TABLE IF EXISTS tasks")
+        c.execute("""CREATE TABLE IF NOT EXISTS tasks (
+                                    id integer PRIMARY KEY,
+                                    path text NOT NULL,
+                                    hash blob);""")
+except Error as e:
+    print(e)
+
+i = 0
+for root, dirs, files in os.walk(path, topdown=False):
+    for filename in files:
+        i = i+1
+        print("Processing file no: ", i)
+        if last_file:
+            if filename == last_file:
+                last_file = None
+
+            print("Skipping file: ", filename)
+            continue
+
+        print('Processing file: ', filename)
+        try:
+            ext = filename.split(".")[-1].lower()
+            if ext in ALLOWED_EXTENSIONS:
+                filename_lower = filename.lower()
+                is_filename_valid = True
+                for ignore_filename_keyword in IGNORE_FILENAME_KEYWORDS:
+                    if ignore_filename_keyword.lower() in filename_lower:
+                        filename_valid = False
+                        break
+                if is_filename_valid:
+                    print('Filename is valid')
+                    path_for_text = os.path.join(path, filename)
+                    text = get_text(path_for_text)
+                    text = text.lower()
+
+                    is_filecontent_valid = True
+                    for ignore_filecontent_keyword in IGNORE_FILECONTENT_KEYWORDS:
+                        if ignore_filecontent_keyword.lower() in text:
+                            is_filecontent_valid = False
+                            break
+
+                    if is_filecontent_valid:
+                        print('File content is valid')
+                        filepath = os.path.join(path, filename)
+                        sqlite_insert_with_param = """INSERT INTO 'tasks'
+                                                  ('id', 'path', 'hash')
+                                                  VALUES (?, ?, ?);"""
+
+                        hash_md5 = hashlib.md5()
+                        digest = None
+                        with open(filepath, "rb") as f:
+                            for chunk in iter(lambda: f.read(4096), b""):
+                                hash_md5.update(chunk)
+                            digest = hash_md5.hexdigest()
+                        c.execute("SELECT * FROM tasks WHERE hash=?", (digest,))
+                        rows = c.fetchall()
+                        if rows:
+                           import pdb; pdb.set_trace()
+                        else:
+                            data_tuple = (i, path_for_text, digest)
+                            c.execute(sqlite_insert_with_param, data_tuple)
+                            conn.commit()
+                            f = open(filepath, 'rb')
+                            files = {'upload_file': f}
+                            r = requests.post(url, files=files)
+                            print(colored(("Status Code is :%s" % r.status_code), 'green'))
+                            f.close()
+                    else:
+                        print("Ignoring file : " + filename + " Invalid filename Content")
+
+                else:
+                    print("Ignoring file : "+ filename + " Invalid filename Keywords" )
+
+            else:
+                print("Ignoring file: "+ filename )
+        except textract.exceptions.ExtensionNotSupported as e:
+            print(e)
+        except textract.exceptions.ShellError as e:
+            print(e)
+        except selenium.common.exceptions.TimeoutException as e:
+            print(e)
+    conn.close()
+
