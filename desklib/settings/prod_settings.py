b81c9200ac0e8a5518ba6801739cd15822f3f7a2
kushagra <kushagra.goel@locusrags.com>
2019-10-15 14:25:40 +0530

    removed filter in logging

--- desklib/settings/prod_settings.py
+++ desklib/settings/prod_settings.py
@@ -83,7 +83,6 @@ LOGGING = {
         'mail_admins': {
             'level': 'ERROR',
             'class': 'django.utils.log.AdminEmailHandler',
-            'filters': ['special']
         }
     },
     'loggers': {
