2266c7832ef0e207b1a45e53f377502e082f0507
kushagra <kushagra.goel@locusrags.com>
2019-10-23 13:37:48 +0530

    removed comming soon url commented redirect middleware

--- desklib/settings/base.py
+++ desklib/settings/base.py
@@ -131,7 +131,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'desklib.middleware.IpRestrict',
+    # 'desklib.middleware.IpRestrict',
 ]
 
 INTERNAL_IPS = [
