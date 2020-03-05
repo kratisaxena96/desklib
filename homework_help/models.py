4949ec82d84bd9c15e429aecdf6ab560626783b9
kushagra <kushagra.goel@locusrags.com>
2020-03-05 10:45:21 +0530

    order chat implemented for homework help

--- homework_help/models.py
+++ homework_help/models.py
@@ -2,6 +2,7 @@ import random
 import string
 
 from django.conf import settings
+from django.core.validators import FileExtensionValidator
 from django.db import models
 from django.utils import timezone
 from subjects.models import Subject
@@ -30,7 +31,8 @@ class Question(models.Model):
     question = models.TextField(_('Question'))
     slug = models.SlugField(_('Slug'), unique=True)
     upload_file = models.FileField(verbose_name=_('Upload File'), upload_to=upload_to, max_length=1000)
-    subjects = models.ForeignKey(Subject, db_index=True, blank=True, null=True, related_name='subject_question', on_delete=models.PROTECT,)
+    subjects = models.ForeignKey(Subject, db_index=True, blank=True, null=True, related_name='subject_question',
+                                 on_delete=models.PROTECT, )
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='author_question',
                                blank=True, null=True)
 
@@ -53,12 +55,12 @@ class Order(models.Model):
     STATUS_CANCELLED = 6
 
     STATUS = [
-    (STATUS_UNASSIGNED, 'Unassigned'),
-    (STATUS_IN_PROGRESS, 'In Progress'),
-    (STATUS_DONE, 'Completed'),
-    (STATUS_DELIVERED, 'Delivered'),
-    (STATUS_FEEDBACK, 'Feedback'),
-    (STATUS_CANCELLED, 'Cancelled'),
+        (STATUS_UNASSIGNED, 'Unassigned'),
+        (STATUS_IN_PROGRESS, 'In Progress'),
+        (STATUS_DONE, 'Completed'),
+        (STATUS_DELIVERED, 'Delivered'),
+        (STATUS_FEEDBACK, 'Feedback'),
+        (STATUS_CANCELLED, 'Cancelled'),
     ]
 
     question = models.ForeignKey(Question, related_name='ordered_question', on_delete=models.PROTECT)
@@ -82,7 +84,9 @@ class Order(models.Model):
 
 class Comment(models.Model):
     message = models.TextField(_('Message'))
-    reference_files = models.FileField(_('Reference File'), upload_to=upload_to, blank=True, null=True)
+    reference_files = models.FileField(_('Reference File'), upload_to=upload_to, blank=True, null=True,
+                                       validators=[
+                                           FileExtensionValidator(allowed_extensions=['png', 'jpg', 'jpeg', 'gif'])])
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='comment_author')
     order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='comment_author')
 
--- homework_help/serializers.py
+++ homework_help/serializers.py
@@ -1,11 +1,21 @@
+from django.core.validators import FileExtensionValidator
 from rest_framework import serializers
 from homework_help.models import Comment
 
 
 class CommentCreateSerializer(serializers.ModelSerializer):
-    reference_files = serializers.FileField(required=False)
-    message = serializers.CharField()
+    reference_files = serializers.FileField(required=False, validators=[FileExtensionValidator(allowed_extensions=['png', 'jpg', 'jpeg', 'gif'])])
+    message = serializers.CharField(required=False)
 
     class Meta:
         model = Comment
         fields = ('message', 'reference_files')
+
+    def validate(self, data):
+        reference_files = data.get('reference_files')
+        message = data.get('message')
+
+        if not reference_files and not message:
+            raise serializers.ValidationError("Fill any one field either message or file.")
+
+        return data
