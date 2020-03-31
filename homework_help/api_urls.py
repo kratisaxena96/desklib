b066a0e66abbad79d22741f06a8e6c28f1ab7209
kushagra <kushagra.goel@locusrags.com>
2020-03-31 19:20:37 +0530

    get answer api implemented

--- homework_help/api_urls.py
+++ homework_help/api_urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from homework_help.api_views import QuestionCreateApiView, CommentCreateApiView, QuestionFileCreateApiView, \
-    OrderStatusApi
+    OrderStatusApi, GetAnswerApiView
 
 urlpatterns = [
     path('create-question/', QuestionCreateApiView.as_view(), name='question-create-api'),
     path('create-question-file/', QuestionFileCreateApiView.as_view(), name='question-file-create-api'),
     path('create/<order_id>/', CommentCreateApiView.as_view(), name='comment-create-api'),
+    path('get-answer/<uid>/', GetAnswerApiView.as_view(), name='get-answer-api'),
     path('<uuid>/', OrderStatusApi.as_view(), name='order-status-api'),
 
 ]
