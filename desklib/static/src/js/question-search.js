a1da1d0101c175b01ed36be32b21b52d1ee4ce1a
Menka Kumari <nishi.kumari@zucolservices.com>
2020-05-08 15:04:08 +0530

    changes in js file

--- desklib/static/src/js/question-search.js
+++ desklib/static/src/js/question-search.js
@@ -1,27 +1,36 @@
+// $(document).ready(function(){
+//        var results = new Bloodhound({
+//            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('question'),
+//            queryTokenizer: Bloodhound.tokenizers.whitespace,
+//            remote: {
+//                url: '/homework-help/autocomplete/?q=%QUERY',
+//                 wildcard: '{query}'
+//            }
+//        });
+//        results.initialize();
 //
-// var search = new Bloodhound({
-//  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
-//  queryTokenizer: Bloodhound.tokenizers.whitespace,
-//  // `states` is an array of state names defined in "The Basics"
-////  prefetch: '/study/autocomplete/?q=ass',
-//
-//  question: {
-//    url: '/homework-help/autocomplete/?q=%QUERY',
-//    wildcard: '%QUERY'
-//  }
-//});
-//
-//$('#question .typeahead').typeahead(null,{
-//
-//  name: 'search',
-//  display: 'title',
-//  source: search,
-//  templates: {
-//    empty: [
-//      '<div class="empty-message">',
-//        'No results found',
-//      '</div>'
-//    ].join('\n'),
-//		suggestion: Handlebars.compile('<a href="/homework-help/question/{{slug}}">{{title}}</a>')
-//  }
-//});
+//        $('#question .typeahead').typeahead({
+//            hint: true,
+//            highlight: true,
+//            minLength: 2,
+//            maxItem: 5,
+//          },
+//          {
+//            name: 'question',
+//            displayKey: 'question',
+//            source: results.ttAdapter(),
+//            templates: {
+//                empty: function(){
+//                    return [
+//                        '<div class="empty-message">',
+//                            'No results found',
+//                        '</div>'].join('\n')
+//                },
+//                suggestion: function(question){
+//                    var string = '<a class="tt-suggestion-link" href="/homework-help/question/'+ question.slug +'">'+ question.question +'</a>'
+//                    return string;
+//                }
+//            }
+//           }
+//        );
+//    });
