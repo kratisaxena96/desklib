9a6acae997c7aaf2229571e85fa62fd28788cf48
Sandhya <prashant.pandey@zucolservices.com>
2020-08-11 17:45:45 +0530

    doc upload paid plan redigned

--- uploads/admin.py
+++ uploads/admin.py
@@ -44,11 +44,10 @@ def make_subscription(modeladmin, request, queryset):
         doc = document.required_document.slug
     user = document.author
     plan = Plan.objects.get(is_pay_per_download=True)
-    doc_file = Document.objects.get(slug=doc)
     create = PayPerDocument.objects.create(user=user, expire_on=datetime.now() + timedelta(days=30),
                                            plan=plan, start_date=datetime.now())
-    create.documents.add(doc_file)
-    context = {'document': doc_file, 'user':user}
+    create.documents.add(document.required_document)
+    context = {'document': document.required_document, 'user':user}
     htmly = render_to_string('subscription/mail-templates/make_subscription.html',
                              context=context, request=None)
     html_message = htmly
