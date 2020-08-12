1911877e7f032ca3c6638076bebfdd36a4ca93b3
Sandhya <prashant.pandey@zucolservices.com>
2020-08-12 14:46:00 +0530

    changes fixed

--- uploads/admin.py
+++ uploads/admin.py
@@ -42,7 +42,6 @@ from subjects.models import Subject
 def make_subscription(modeladmin, request, queryset):
     for document in queryset:
         doc = document.required_document.slug
-        doc_file = document.required_document
     user = document.author
     plan = Plan.objects.get(is_pay_per_download=True)
     create = PayPerDocument.objects.create(user=user, expire_on=datetime.now() + timedelta(days=30),
