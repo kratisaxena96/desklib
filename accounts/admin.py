ee63a3c92ef073c2aa5abe84705ed28a1181d75f
kushagra <kushagra.goel@locusrags.com>
2020-09-30 16:19:20 +0530

    user account admin improved

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 from accounts.models import UserAccount
+from django import forms
+from django.contrib.auth.models import Permission
+from django.contrib.admin.widgets import FilteredSelectMultiple
 
 # Register your models here.
 
+class UserAccountForm(forms.ModelForm):
+    user_permissions = forms.ModelMultipleChoiceField(queryset=Permission.objects.all().prefetch_related('content_type'), widget=FilteredSelectMultiple("Option", is_stacked=False), required=False)
+
+    class Meta:
+        model = UserAccount
+        exclude = []
+
+
 class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'full_name', 'email', 'contact_no', 'is_active')
     search_fields = ['email']
+    form = UserAccountForm
+
+    def full_name(self, obj):
+        return obj.get_full_name()
+
 
 admin.site.register(UserAccount, UserAdmin)
