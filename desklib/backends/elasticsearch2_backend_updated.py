37c532a5ff710d34794644f2efdc2f828d44aae5
kushagra <kushagra.goel@locusrags.com>
2020-10-08 10:47:00 +0530

    changed paypal mail updated elasticsearch update

---
+++ desklib/backends/elasticsearch2_backend_updated.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from haystack.backends import BaseEngine
+from haystack.backends.elasticsearch_backend import ElasticsearchSearchBackend
+from haystack.backends.elasticsearch2_backend import Elasticsearch2SearchQuery
+from haystack.constants import ID
+from haystack.utils import get_identifier
+from haystack.exceptions import MissingDependency, SkipDocument
+
+try:
+    import elasticsearch
+    if not ((2, 0, 0) <= elasticsearch.__version__ < (3, 0, 0)):
+        raise ImportError
+    from elasticsearch.helpers import bulk, scan
+except ImportError:
+    raise MissingDependency("The 'elasticsearch2' backend requires the \
+                            installation of 'elasticsearch>=2.0.0,<3.0.0'. \
+                            Please refer to the documentation.")
+
+
+class Elasticsearch2SearchBackendUpdated(ElasticsearchSearchBackend):
+    def __init__(self, connection_alias, **connection_options):
+        super(Elasticsearch2SearchBackendUpdated, self).__init__(connection_alias, **connection_options)
+        self.content_field_name = None
+
+
+    def update(self, index, iterable, commit=True):
+        if not self.setup_complete:
+            try:
+                self.setup()
+            except elasticsearch.TransportError as e:
+                if not self.silently_fail:
+                    raise
+
+                self.log.error("Failed to add documents to Elasticsearch: %s", e, exc_info=True)
+                return
+
+        prepped_docs = []
+
+        for obj in iterable:
+            try:
+                prepped_data = index.full_prepare(obj)
+                final_data = {}
+
+                # Convert the data to make sure it's happy.
+                for key, value in prepped_data.items():
+                    final_data[key] = self._from_python(value)
+                final_data['_id'] = final_data[ID]
+
+                prepped_docs.append(final_data)
+                print(obj.slug)
+            except SkipDocument:
+                self.log.debug(u"Indexing for object `%s` skipped", obj)
+            except elasticsearch.TransportError as e:
+                if not self.silently_fail:
+                    raise
+
+                # We'll log the object identifier but won't include the actual object
+                # to avoid the possibility of that generating encoding errors while
+                # processing the log message:
+                self.log.error(u"%s while preparing object for update" % e.__class__.__name__, exc_info=True,
+                               extra={"data": {"index": index,
+                                               "object": get_identifier(obj)}})
+
+        bulk(self.conn, prepped_docs, index=self.index_name, doc_type='modelresult')
+
+        if commit:
+            self.conn.indices.refresh(index=self.index_name)
+
+
+class Elasticsearch2SearchQueryUpdated(Elasticsearch2SearchQuery):
+    pass
+
+
+class Elasticsearch2SearchUpdatedEngine(BaseEngine):
+    backend = Elasticsearch2SearchBackendUpdated
+    query = Elasticsearch2SearchQueryUpdated
--- documents/views.py
+++ documents/views.py
@@ -513,7 +513,7 @@ class DocumentPayment(LoginRequiredMixin, MetadataMixin, TemplateView):
         if settings.PAYPAL_TEST:
             receiver_email = "info-facilitator@a2zservices.net"
         else:
-            receiver_email = "payment@locusrags.com"
+            receiver_email = "info@a2zservices.net"
         paypal_dict = {
             "business": receiver_email,
             "item_name": "desklib subscription",
