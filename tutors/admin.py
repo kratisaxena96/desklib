a90c1451a7009c5dac897439eb224e03376727bc
sandhya14 <sandhya.k@zucol.in>
2021-02-22 11:16:40 +0530

    updated doc_payment redesign ui

--- tutors/admin.py
+++ tutors/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from tutors.models import Tutor
+from tutors.models import Tutor, Skills
 
 admin.site.register(Tutor)
+admin.site.register(Skills)
--- tutors/models.py
+++ tutors/models.py
@@ -8,16 +8,26 @@ def img_uploads(instance, filename):
     return 'tutors/upload/{}'.format(filename)
 
 
+class Skills(models.Model):
+    skill = models.CharField(verbose_name="skill", max_length=50)
+    # created = models.DateTimeField(auto_now_add=True)
+    # updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.skill
+
+
 class Tutor(models.Model):
     slug = models.SlugField(unique=True, max_length=60)
     profile_pic = models.ImageField(verbose_name='profile_pic', upload_to=img_uploads, max_length=500, blank=True)
     name = models.CharField(max_length=200)
     designation = models.CharField(verbose_name="designation", max_length=200)
     rating = models.IntegerField(verbose_name="rating")
-    review_content = models.TextField(verbose_name="review_content")
+    review_count = models.PositiveIntegerField(verbose_name="review_count")
     description = models.TextField()
     price = models.PositiveIntegerField(verbose_name="price")
     subjects = models.ForeignKey(Subject, related_name="subjects", blank=True, null=True, on_delete=models.SET_NULL)
+    skills = models.ManyToManyField(Skills, related_name="skills", blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -25,4 +35,4 @@ class Tutor(models.Model):
         return self.designation
 
     def get_absolute_url(self):
-        return reverse('tutors:tutor-detail',kwargs={'slug':self.slug})
+        return reverse('tutors:tutor-detail', kwargs={'slug': self.slug})
